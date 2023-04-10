7c0b30f79d6b49491ad638a6807e343c0f368594
Anastasiya Turenko <oxtnpi@gmail.com>
2023-04-10 16:09:06 +0600

    add Leaflet library

--- starter/script.js
+++ starter/script.js
@@ -11,12 +11,26 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
-if(navigator.geolocation){
-    navigator.geolocation.getCurrentPosition(function(position){
-       const {latitude} = position.coords;
-       const {longitude} = position.coords;
-       
-    },function(){
-        alert('Could not get your position')
-    })
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+      const coords = [latitude,longitude];
+      const map = L.map('map').setView(coords, 13);
+      
+      L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker(coords)
+        .addTo(map)
+        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+        .openPopup();
+    },
+    function () {
+      alert('Could not get your position');
+    }
+  );
 }
